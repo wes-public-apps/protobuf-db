2fa0cb77bcad3e99a542aa303f9cce2c87383ffc
Wesley Murray <wesley@guardia.ag>
2022-08-20 13:06:47 -0400

    initialize app structure

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+tests_requires = []
+
+dev_requires = [] + tests_requires
+
+setup(
+    name="protobuf-db",
+    version="1.0.0",
+    description="A python utility for managing protobuf database objects.",
+    author='Wesley Murray',
+    author_email='murraywj97@gmail.com',
+    url='https://github.com/wes-public-apps/protobuf-db',
+    download_url="https://github.com/wes-public-apps/protobuf-db/releases",
+    license='MIT',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="protobuf schema sql migration",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[],
+    tests_require=tests_requires,
+    extras_require={
+        'test': tests_requires,
+        'dev': dev_requires,
+    },
+    include_package_data=True,
+)
