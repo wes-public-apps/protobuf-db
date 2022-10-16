dc936e0ca8dc545b26f224986e1d9ebafb930d79
Wesley Murray <wesley@guardia.ag>
2022-10-15 22:08:27 -0400

    [transforms][graphql] generate client query string

---
+++ protobuf_utility/transforms/graphql_transformer.py
@@ -0,0 +1,45 @@
+import google
+from google.protobuf.descriptor import FieldDescriptor
+
+MessageMeta = google._upb._message.MessageMeta
+ScalarMapContainer = google._upb._message.ScalarMapContainer
+MessageMapContainer = google._upb._message.MessageMapContainer
+Descriptor = google._upb._message.Descriptor
+
+
+def proto_definition_to_graphql_query(proto_def: MessageMeta) -> str:
+    """Generated the client side graphql query for protobuf object as a string.
+    Args:
+        proto_def (MessageMeta): protobuf class to convert to a query.
+    Returns:
+        str: protobuf query
+    """
+    return _proto_definition_to_graphql_query(proto_def.DESCRIPTOR)
+
+
+def _proto_definition_to_graphql_query(proto_descriptor: Descriptor, depth=1):
+    # Sort fields based on number to provide a more reproducible order
+    # Unsorted order matches that of file definition order even though there is no change to the
+    # underlying protobuf if a field is moved. This makes the code unnecessarily unstable.
+    field_nums = []
+    fields = []
+    for field in proto_descriptor.fields:
+        field_nums.append(field.number)
+        fields.append(field)
+    id_fields = zip(field_nums, fields)
+    sorted_id_fields = sorted(id_fields)
+
+    query = '{'
+    for _, field in sorted_id_fields:
+        tab_str = "\t"*depth
+
+        # Works completely for: scalars, list of scalars
+        # Forms base for: nest messages, repeated messages, dictionaries
+        query += f'\n{tab_str}{field.name}'
+
+        # Handle nested messages
+        if field.type == FieldDescriptor.TYPE_MESSAGE:
+            query += ' '
+            query += _proto_definition_to_graphql_query(field.message_type, depth=depth+1)
+
+    return f'{query}\n{tab_str[:-1]}' + '}'
---
+++ tests/test_graphql_transformer.py
@@ -0,0 +1,67 @@
+from unittest import TestCase
+import unittest
+
+import test_data as td
+
+from protobuf_utility.transforms.graphql_transformer import proto_definition_to_graphql_query
+
+
+class TestGraphqlTransformer(TestCase):
+
+    def test_proto_definition_to_graphql_query_raw(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.raw_msg),
+            '{\n\tid\n\ttimestamp\n\tdata\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_n4(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.n4),
+            '{\n\tid\n\traw_msgs {\n\t\tid\n\t\ttimestamp\n\t\tdata\n\t}\n\traw_msgs_by_id {\n\t\tkey\n\t\tvalue {\n\t\t\tid\n\t\t\ttimestamp\n\t\t\tdata\n\t\t}\n\t}\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_complex(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.complex_msg),
+            '{\n\traw_msgs_by_id {\n\t\tkey\n\t\tvalue {\n\t\t\tid\n\t\t\ttimestamp\n\t\t\tdata\n\t\t}\n\t}\n\tn4s {\n\t\tid\n\t\traw_msgs {\n\t\t\tid\n\t\t\ttimestamp\n\t\t\tdata\n\t\t}\n\t\traw_msgs_by_id {\n\t\t\tkey\n\t\t\tvalue {\n\t\t\t\tid\n\t\t\t\ttimestamp\n\t\t\t\tdata\n\t\t\t}\n\t\t}\n\t}\n\tn4s_by_id {\n\t\tkey\n\t\tvalue {\n\t\t\tid\n\t\t\traw_msgs {\n\t\t\t\tid\n\t\t\t\ttimestamp\n\t\t\t\tdata\n\t\t\t}\n\t\t\traw_msgs_by_id {\n\t\t\t\tkey\n\t\t\t\tvalue {\n\t\t\t\t\tid\n\t\t\t\t\ttimestamp\n\t\t\t\t\tdata\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n\tn5s_by_id {\n\t\tkey\n\t\tvalue {\n\t\t\ttypes\n\t\t\tdata\n\t\t}\n\t}\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_n6(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.n6),
+            '{\n\tn5val {\n\t\ttypes\n\t\tdata\n\t}\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_n2(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.n2),
+            '{\n\tval2\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_nested(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.test_nested),
+            '{\n\tval1 {\n\t\tval1\n\t}\n\tval2 {\n\t\tval2\n\t}\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_specials(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.test_specials),
+            '{\n\tlist1\n\tmap1 {\n\t\tkey\n\t\tvalue\n\t}\n\tfault1\n\tfault2\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_n3(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.n3),
+            '{\n\tval1\n\tval2\n}'
+        )
+
+    def test_proto_definition_to_graphql_query_types(self) -> None:
+        self.assertEqual(
+            proto_definition_to_graphql_query(td.test_types),
+            '{\n\tval1\n\tval2\n\tval3\n\tval4\n\tval5\n\tval6\n\tval7\n\tval8\n\tval9\n\tval10\n\tval11\n\tval12\n\tval13\n\tval14\n\tval15\n\tval16\n\tval17 {\n\t\tval1\n\t\tval2\n\t}\n\tval18 {\n\t\tkey\n\t\tvalue {\n\t\t\tval1\n\t\t\tval2\n\t\t}\n\t}\n\tval19 {\n\t\tval1\n\t\tval2\n\t}\n}'
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
